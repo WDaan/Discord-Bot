aa8a681c1572f4258d8479df87edadc8487c0cd1
Daan Wijns <dw.daanwijns@gmail.com>
2020-09-24 13:16:09 +0200

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,18 +4,5 @@ module.exports = {
             name: 'BOT',
             script: 'dist/main.js'
         }
-    ],
-    deploy: {
-        production: {
-            user: 'pi',
-            host: ['10.0.0.2'],
-            ref: 'origin/master',
-            repo: 'git@github.com:WDaan/Discord-Bot.git',
-            path: '/home/pi/Documents/Github/Discord-Bot',
-            'pre-deploy-local': '',
-            'post-deploy':
-                'npm install && npm run build && pm2 reload ecosystem.config.js --env production',
-            'pre-setup': ''
-        }
-    }
+    ]
 }
